738b61ada3c312021a36d95b3e40c4070ee58f94
DanielRasho <ray22933@uvg.edu.gt>
2024-12-05 23:09:34 -0600

    feat: define structures and method to load environment files

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,29 +4,80 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 )
 
 type Config struct {
-	AppName    string
-	Port       int
+	APIPort     string
+	RunningMode string
+
+	// Database
+	DBHost     string
+	DBPort     string
 	DBUser     string
 	DBPassword string
-	DBHost     string
 	DBName     string
-	AuthSecret string
+
+	// Cors
+	AllowedOrigins      []string
+	AllowedContentTypes []string
+	AllowedMethods      []string
+	AllowedHeaders      []string
+
+	// Logging
+	LoggingMethod   string
+	LoggingFilePath string
+
+	// Auth0
+	AuthClientId      string
+	AuthClientSecret  string
+	AuthAudience      string
+	AuthIssuerBaseUrl string
+
+	// S3
+	AwsAccessKeyID     string
+	AwsSecretAccessKey string
+	AwsRegion          string
+	AwsBucketName      string
+	AwsBucketNameTest  string
 }
 
 // LoadConfig loads configuration from environment variables.
 // It exits with a fatal error if any required variable is missing.
 func LoadConfig() Config {
 	return Config{
-		AppName:    mustGetEnv("APP_NAME"),
-		Port:       mustGetEnvAsInt("APP_PORT"),
-		DBUser:     mustGetEnv("DB_USER"),
-		DBPassword: mustGetEnv("DB_PASSWORD"),
+		APIPort:     mustGetEnv("API_PORT"),
+		RunningMode: mustGetEnv("RUNNING_MODE"),
+
+		// Database
 		DBHost:     mustGetEnv("DB_HOST"),
+		DBPort:     mustGetEnv("DB_PORT"),
+		DBUser:     mustGetEnv("DB_USER"),
+		DBPassword: mustGetEnv("DB_USER_PASSWORD"),
 		DBName:     mustGetEnv("DB_NAME"),
-		AuthSecret: mustGetEnv("AUTH_SECRET"),
+
+		// Cors
+		AllowedOrigins:      mustGetEnvAsStringSlice("ALLOWED_ORIGINS"),
+		AllowedContentTypes: mustGetEnvAsStringSlice("ALLOWED_CONTENT_TYPES"),
+		AllowedMethods:      mustGetEnvAsStringSlice("ALLOWED_METHODS"),
+		AllowedHeaders:      mustGetEnvAsStringSlice("ALLOWED_HEADERS"),
+
+		// Logging
+		LoggingMethod:   mustGetEnv("LOGGING_METHOD"),
+		LoggingFilePath: mustGetEnv("LOGGING_FILE_PATH"),
+
+		// AUTH0
+		AuthClientId:      mustGetEnv("AUTH_CLIENT_ID"),
+		AuthClientSecret:  mustGetEnv("AUTH_CLIENT_SECRET"),
+		AuthAudience:      mustGetEnv("AUTH_AUDIENCE"),
+		AuthIssuerBaseUrl: mustGetEnv("AUTH_ISSUER_BASE_URL"),
+
+		// S3
+		AwsAccessKeyID:     mustGetEnv("AWS_ACCESS_KEY_ID"),
+		AwsSecretAccessKey: mustGetEnv("AWS_SECRET_ACCESS_KEY"),
+		AwsRegion:          mustGetEnv("AWS_REGION"),
+		AwsBucketName:      mustGetEnv("AWS_BUCKET_NAME"),
+		AwsBucketNameTest:  mustGetEnv("AWS_BUCKET_NAME_TEST"),
 	}
 }
 
@@ -50,4 +101,13 @@ func mustGetEnvAsInt(key string) int {
 		log.Fatalf("Environment variable %s must be a valid integer: %v", key, err)
 	}
 	return value
+}
+
+func mustGetEnvAsStringSlice(key string) []string {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		log.Fatalf("Environment variable %s is required but not set", key)
+	}
+
+	return strings.Split(value, ",")
 }
