a24161bdee68932c6a976822288ab7d842e012ed
DanielRasho <ray22933@uvg.edu.gt>
2024-12-05 23:10:09 -0600

    feat: first usable API with logging and cors

--- main.go
+++ main.go
@@ -1,15 +1,41 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/TherapistTrack/Therapisttrack-Backend-V2/internal/api/handlers/health"
+	"github.com/TherapistTrack/Therapisttrack-Backend-V2/internal/api/middlewares"
+	"github.com/TherapistTrack/Therapisttrack-Backend-V2/internal/config"
+	"github.com/TherapistTrack/Therapisttrack-Backend-V2/pkg"
+	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		fmt.Println("Could not load .env file!")
-	}
-	fmt.Println("Hello world")
+	// Loading env variables from .env file
+	godotenv.Load()
+	config := config.LoadConfig()
+
+	// Logging
+	pkg.ConfigureLogger(pkg.LoggerConfig{
+		Output: config.LoggingMethod,
+		Path:   config.LoggingFilePath,
+	})
+
+	// Routes
+	r := chi.NewRouter()
+
+	r.Use(middlewares.LoggingMiddleware)
+	r.Use(middlewares.CreateCorsMiddleware(
+		config.AllowedOrigins,
+		config.AllowedMethods,
+		config.AllowedHeaders,
+	))
+
+	r.Get("/health", health.CheckHealthHandler)
+
+	// Start server
+	log.Printf("Starting server on port %s", config.APIPort)
+	log.Fatal(http.ListenAndServe(":"+config.APIPort, r))
 }
