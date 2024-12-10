8ac27cdd0c1a7feb669ebb7e1fb05017a63f0fd8
DanielRasho <ray22933@uvg.edu.gt>
2024-12-09 22:16:25 -0600

    fix: add missing fields for User Model

--- internal/storage/mongo/models.go
+++ internal/storage/mongo/models.go
@@ -3,17 +3,18 @@ package mongo
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type UserModel struct {
-	Id                primitive.ObjectID `bson:"_id"`
+	Id                primitive.ObjectID `bson:"_id,omitempty"`
 	Names             string             `bson:"names"`
 	LastNames         string             `bson:"lastNames"`
 	Mails             []string           `bson:"mails"`
 	Phones            []string           `bson:"phones"`
-	Role              []string           `bson:"role"`
+	Role              string             `bson:"rol"`
 	RoleDependentInfo primitive.ObjectID `bson:"roleDependentInfo"`
+	IsActive          bool               `bson:"isActive"`
 }
 
 type DoctorModel struct {
-	Id               primitive.ObjectID `bson:"_id"`
+	Id               primitive.ObjectID `bson:"_id,omitempty"`
 	User             primitive.ObjectID `bson:"user"`
 	Specialty        string             `bson:"specialty"`
 	CollegiateNumber string             `bson:"collegiateNumber"`
