cd33f0200a99b79137bf153aa1348eeb11856ac5
DanielRasho <ray22933@uvg.edu.gt>
2024-12-10 19:24:39 -0600

    feat: add defensive closures for createUser endpoint

--- api/handlers/user/create_user.go
+++ api/handlers/user/create_user.go
@@ -5,64 +5,103 @@ import (
 	"fmt"
 	"net/http"
 
+	md "github.com/TherapistTrack/Therapisttrack-Backend-V2/api/middlewares"
 	"github.com/TherapistTrack/Therapisttrack-Backend-V2/internal/services"
 	"github.com/TherapistTrack/Therapisttrack-Backend-V2/internal/services/user"
 	"github.com/TherapistTrack/Therapisttrack-Backend-V2/pkg"
 	"github.com/rs/zerolog/log"
 )
 
+type createUserRequest struct {
+	Id                string   `json:"id"`
+	Names             string   `json:"names" validate:"required"`
+	LastNames         string   `json:"lastNames" validate:"required"`
+	Phones            []string `json:"phones" validate:"required"`
+	Mails             []string `json:"mails" validate:"required"`
+	Role              string   `json:"rol" validate:"required,oneof=Doctor Assistant Admin"`
+	RoleDependentInfo struct {
+		CollegiateNumber string `json:"collegiateNumber,omitempty"`
+		Specialty        string `json:"specialty,omitempty"`
+		StartDate        string `json:"startDate,omitempty"`
+		EndDate          string `json:"endDate,omitempty"`
+		DPI              string `json:"DPI,omitempty" validate:"omitempty,numeric"`
+	}
+}
+
+type createUserResponse struct {
+	Message string `json:"message"`
+	UserId  string `json:"userId"`
+	RoleId  string `json:"roleId"`
+}
+
 // NewCreateUserHandler handles the /user endpoint.
 func NewCreateUserHandler(s *user.UserService) http.HandlerFunc {
 
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		failedValidation := false
+		failedJSONValidation := ""
 
-		// Decode the request body into createUserRequest
-		var data services.User
-		// Validate correct types
+		var data createUserRequest
+
 		if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
-			failedValidation = true
+			failedJSONValidation = "Request fields mismatch types"
 		}
 
-		// Validate all fields sent
 		if err := s.Validator.Struct(data); err != nil {
-			failedValidation = true
+			failedJSONValidation = "Request Missing fields, not all fields sent"
 		}
 
 		// Validating Role Dependent Info based on the Role field.
 		switch roleInfo := data.RoleDependentInfo; data.Role {
 		case "Doctor":
 			if roleInfo.CollegiateNumber == "" || data.RoleDependentInfo.Specialty == "" {
-				failedValidation = true
+				failedJSONValidation = "Collegiate Number or Specialty must not be empty"
 			}
 		case "Assistant":
 			if len(roleInfo.DPI) != 13 ||
 				!pkg.IsValidISO8601(roleInfo.StartDate) ||
 				!pkg.IsValidISO8601(roleInfo.EndDate) {
-				failedValidation = true
+				failedJSONValidation = "Date is not in ISO8601 Format"
 			}
 		}
 
-		if failedValidation {
-			http.Error(w, fmt.Sprintf(`{"message": "%s"}`, pkg.MissingFields), http.StatusBadRequest)
+		if len(failedJSONValidation) > 0 {
+			body, _ := json.Marshal(data)
+			md.LogErrorRequest(fmt.Errorf(failedJSONValidation), r, pkg.MissingFields.StatusCode, &body)
+			http.Error(w, fmt.Sprintf(`{"message": "%s"}`, pkg.MissingFields), pkg.MissingFields.StatusCode)
 			return
 		}
 
-		// Create User
-		userId, roleId, err := s.CreateDoctor(r.Context(), &data)
+		log.Debug().Msg("Creating User...")
+
+		user := services.User{
+			Id:        data.Id,
+			Names:     data.Names,
+			LastNames: data.LastNames,
+			Phones:    data.Phones,
+			Mails:     data.Mails,
+			Role:      data.Role,
+			RoleDependentInfo: services.RoleDependentInfo{
+				CollegiateNumber: data.RoleDependentInfo.CollegiateNumber,
+				Specialty:        data.RoleDependentInfo.Specialty,
+				StartDate:        data.RoleDependentInfo.StartDate,
+				EndDate:          data.RoleDependentInfo.EndDate,
+				DPI:              data.RoleDependentInfo.DPI}}
+
+		userId, roleId, err := s.CreateUser(r.Context(), &user)
 		if err != nil {
-			log.Debug().Msg(err.Error())
+			body, _ := json.Marshal(data)
+			md.LogErrorRequest(err, r, http.StatusBadRequest, &body)
 			http.Error(w, fmt.Sprintf(`{"message": "%s"}`, pkg.MissingFields), http.StatusBadRequest)
 			return
 		}
 
 		// Respond with success
-		w.WriteHeader(http.StatusCreated)
-		json.NewEncoder(w).Encode(map[string]string{
-			"message": pkg.RequestSuccess,
-			"userId":  userId,
-			"roleId":  roleId})
+		w.WriteHeader(pkg.RequestSuccess.StatusCode)
+		json.NewEncoder(w).Encode(createUserResponse{
+			Message: pkg.RequestSuccess.Message,
+			UserId:  userId,
+			RoleId:  roleId})
 	}
 
 }
--- internal/services/user/create_user.go
+++ internal/services/user/create_user.go
@@ -2,49 +2,58 @@ package user
 
 import (
 	"context"
+	"time"
 
 	"github.com/TherapistTrack/Therapisttrack-Backend-V2/internal/services"
-	"github.com/TherapistTrack/Therapisttrack-Backend-V2/internal/storage/mongo"
+	"github.com/TherapistTrack/Therapisttrack-Backend-V2/internal/storage/mongo_cli"
+	"github.com/TherapistTrack/Therapisttrack-Backend-V2/pkg"
 	"github.com/rs/zerolog/log"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func (s *UserService) CreateDoctor(ctx context.Context, user *services.User) (string, string, error) {
+func (s *UserService) CreateUser(ctx context.Context, user *services.User) (string, string, error) {
 	// Collections
 	userColl := s.dbClient.DB.Collection("User")
-	doctorColl := s.dbClient.DB.Collection("Doctor")
 
-	// Insert user into the User collection
-	dbUser := mongo.UserModel{
+	dbUser := mongo_cli.UserModel{
 		Names:     user.Names,
 		LastNames: user.LastNames,
 		Phones:    user.Phones,
 		Mails:     user.Mails,
-		Role:      "Doctor",
+		Role:      user.Role,
 		IsActive:  true,
 	}
 
+	log.Debug().Msg("Insert User on DB")
 	userResult, err := userColl.InsertOne(ctx, dbUser)
 	if err != nil {
-		return "", "", err
+		if mongo.IsDuplicateKeyError(err) {
+			return "", "", pkg.ResourceWithNameAlreadyExist
+		}
+		return "", "", pkg.BadDatabaseOperation
 	}
 
-	// Insert the doctor data into the Doctor collection
-	dbRole := mongo.DoctorModel{
-		User:             userResult.InsertedID.(primitive.ObjectID),
-		CollegiateNumber: user.RoleDependentInfo.CollegiateNumber,
-		Specialty:        user.RoleDependentInfo.Specialty,
+	log.Debug().Msg("Create Role Dependent Info")
+	userId := userResult.InsertedID.(primitive.ObjectID)
+	var roleResult *mongo.InsertOneResult
+	switch user.Role {
+	case "Admin":
+		return userId.Hex(), "", nil // If role Admin, NO need to create more, so just return
+	case "Doctor":
+		roleResult, err = createDoctor(ctx, s.dbClient, user, userId)
+		if err != nil {
+			return "", "", err
+		}
+	case "Assistant":
+		roleResult, err = createDoctor(ctx, s.dbClient, user, userId)
+		if err != nil {
+			return "", "", err
+		}
 	}
 
-	roleResult, err := doctorColl.InsertOne(ctx, dbRole)
-	if err != nil {
-		return "", "", err
-	}
-
-	log.Debug().Msg("So good so far")
-
-	// Update the User collection with the Doctor's ID
+	log.Debug().Msg("Update the User collection with the Docto's ID")
 	_, err = userColl.UpdateOne(ctx,
 		bson.M{"_id": userResult.InsertedID.(primitive.ObjectID)},
 		bson.M{"$set": bson.M{"roleDependentInfo": roleResult.InsertedID.(primitive.ObjectID)}})
@@ -52,10 +61,48 @@ func (s *UserService) CreateDoctor(ctx context.Context, user *services.User) (st
 		return "", "", err
 	}
 
-	log.Debug().Msg(roleResult.InsertedID.(primitive.ObjectID).String())
-
-	// Return the inserted IDs
 	return userResult.InsertedID.(primitive.ObjectID).Hex(),
 		roleResult.InsertedID.(primitive.ObjectID).Hex(),
 		nil
+}
+
+func createDoctor(ctx context.Context, dbClient *mongo_cli.MongoClient, user *services.User, userId primitive.ObjectID) (*mongo.InsertOneResult, error) {
+	doctorColl := dbClient.DB.Collection("Doctor")
+
+	dbRole := mongo_cli.DoctorModel{
+		User:             userId,
+		CollegiateNumber: user.RoleDependentInfo.CollegiateNumber,
+		Specialty:        user.RoleDependentInfo.Specialty,
+	}
+
+	roleResult, err := doctorColl.InsertOne(ctx, dbRole)
+	if err != nil {
+		return nil, pkg.BadDatabaseOperation
+	}
+	return roleResult, nil
+}
+
+func createAssistant(ctx context.Context, dbClient *mongo_cli.MongoClient, user *services.User, userId primitive.ObjectID) (*mongo.InsertOneResult, error) {
+	assistantColl := dbClient.DB.Collection("Assistant")
+
+	// Convert ISO8601 strings to time.Time and then to primitive.DateTime
+	startDate, _ := time.Parse(time.RFC3339, user.RoleDependentInfo.StartDate)
+	endDate, _ := time.Parse(time.RFC3339, user.RoleDependentInfo.EndDate)
+
+	if startDate.Before(endDate) {
+		return nil, pkg.DateNotPrevious
+	}
+
+	dbRole := mongo_cli.AssistantModel{
+		User:      userId,
+		StartDate: primitive.NewDateTimeFromTime(startDate),
+		EndDate:   primitive.NewDateTimeFromTime(endDate),
+		DPI:       user.RoleDependentInfo.DPI,
+	}
+
+	roleResult, err := assistantColl.InsertOne(ctx, dbRole)
+	if err != nil {
+		return nil, pkg.BadDatabaseOperation
+	}
+	return roleResult, nil
 }
